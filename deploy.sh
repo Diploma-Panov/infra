06604a30cc1895905f43d9ab60ee4219bfbba165
Maksym Panov <work.maksym.panov@gmail.com>
2025-05-05 12:27:30 +0300

    Fix fluentbit version

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-echo $ACTIONS_DEPLOYER_ARN
-
 echo "Planning & Applying *all* Infrastructure"
 terraform -chdir=aws plan \
   -var="alb_certificate_arn=${ALB_CERTIFICATE_ARN}" \
